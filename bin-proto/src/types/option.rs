d4c824051a1289f09cf520a610f8658e9627a5ce
Wojciech Graj <wojtek.graj.2004@gmail.com>
2024-07-25 08:39:54 +0200

    Implement ExternallyTagged on Option.

--- bin-proto/src/types/option.rs
+++ bin-proto/src/types/option.rs
@@ -1,13 +1,16 @@
-use crate::{BitField, BitRead, BitWrite, ByteOrder, Protocol, Result};
+use crate::{BitRead, BitWrite, ByteOrder, ExternallyTagged, Protocol, Result};
 
-impl<Ctx, T> Protocol<Ctx> for Option<T>
+impl<Ctx, T> ExternallyTagged<bool, Ctx> for Option<T>
 where
     T: Protocol<Ctx>,
 {
-    fn read(read: &mut dyn BitRead, byte_order: ByteOrder, ctx: &mut Ctx) -> Result<Self> {
-        let is_some = <bool as Protocol<Ctx>>::read(read, byte_order, ctx)?;
-
-        if is_some {
+    fn read(
+        read: &mut dyn BitRead,
+        byte_order: ByteOrder,
+        ctx: &mut Ctx,
+        tag: bool,
+    ) -> Result<Self> {
+        if tag {
             let value = T::read(read, byte_order, ctx)?;
             Ok(Some(value))
         } else {
@@ -16,44 +19,6 @@ where
     }
 
     fn write(&self, write: &mut dyn BitWrite, byte_order: ByteOrder, ctx: &mut Ctx) -> Result<()> {
-        Protocol::write(&self.is_some(), write, byte_order, ctx)?;
-
-        if let Some(ref value) = *self {
-            value.write(write, byte_order, ctx)?;
-        }
-        Ok(())
-    }
-}
-
-impl<Ctx, T> BitField<Ctx> for Option<T>
-where
-    T: Protocol<Ctx>,
-{
-    fn read(
-        read: &mut dyn BitRead,
-        byte_order: ByteOrder,
-        ctx: &mut Ctx,
-        bits: u32,
-    ) -> Result<Self> {
-        let is_some = <bool as BitField<Ctx>>::read(read, byte_order, ctx, bits)?;
-
-        if is_some {
-            let value = T::read(read, byte_order, ctx)?;
-            Ok(Some(value))
-        } else {
-            Ok(None)
-        }
-    }
-
-    fn write(
-        &self,
-        write: &mut dyn BitWrite,
-        byte_order: ByteOrder,
-        ctx: &mut Ctx,
-        bits: u32,
-    ) -> Result<()> {
-        BitField::write(&self.is_some(), write, byte_order, ctx, bits)?;
-
         if let Some(ref value) = *self {
             value.write(write, byte_order, ctx)?;
         }
@@ -63,18 +28,20 @@ where
 
 #[cfg(test)]
 mod tests {
-    use std::io::Cursor;
-
     use bitstream_io::{BigEndian, BitReader, BitWriter};
-
-    use crate::ProtocolNoCtx;
 
     use super::*;
 
     #[test]
     fn can_read_some() {
         assert_eq!(
-            Option::<u8>::from_bytes(&[1, 5], ByteOrder::BigEndian).unwrap(),
+            <Option<u8> as ExternallyTagged<_, _>>::read(
+                &mut BitReader::endian([5].as_slice(), BigEndian),
+                ByteOrder::BigEndian,
+                &mut (),
+                true
+            )
+            .unwrap(),
             Some(5)
         )
     }
@@ -82,64 +49,40 @@ mod tests {
     #[test]
     fn can_read_none() {
         assert_eq!(
-            Option::<u8>::from_bytes(&[0], ByteOrder::BigEndian).unwrap(),
+            <Option<u8> as ExternallyTagged<_, _>>::read(
+                &mut BitReader::endian([].as_slice(), BigEndian),
+                ByteOrder::BigEndian,
+                &mut (),
+                false
+            )
+            .unwrap(),
             None
         )
     }
 
     #[test]
     fn can_write_some() {
-        assert_eq!(Some(5u8).bytes(ByteOrder::BigEndian).unwrap(), &[1, 5])
+        let mut data: Vec<u8> = Vec::new();
+        ExternallyTagged::write(
+            &Some(5u8),
+            &mut BitWriter::endian(&mut data, BigEndian),
+            ByteOrder::BigEndian,
+            &mut (),
+        )
+        .unwrap();
+        assert_eq!(data, vec![5])
     }
 
     #[test]
     fn can_write_none() {
-        assert_eq!(None::<u8>.bytes(ByteOrder::BigEndian).unwrap(), &[0])
-    }
-
-    #[test]
-    fn can_read_some_bitfield() {
-        assert_eq!(
-            <Option::<u8> as BitField>::read(
-                &mut BitReader::endian(Cursor::new([0x82u8, 0x80]), BigEndian),
-                ByteOrder::BigEndian,
-                &mut (),
-                1,
-            )
-            .unwrap(),
-            Some(5)
+        let mut data: Vec<u8> = Vec::new();
+        ExternallyTagged::write(
+            &None::<u8>,
+            &mut BitWriter::endian(&mut data, BigEndian),
+            ByteOrder::BigEndian,
+            &mut (),
         )
-    }
-
-    #[test]
-    fn can_read_none_bitfield() {
-        assert_eq!(
-            <Option::<u8> as BitField>::read(
-                &mut BitReader::endian(Cursor::new([0x00]), BigEndian),
-                ByteOrder::BigEndian,
-                &mut (),
-                1,
-            )
-            .unwrap(),
-            None
-        )
-    }
-
-    #[test]
-    fn can_write_some_bitfield() {
-        let mut buffer: Vec<u8> = Vec::new();
-        let mut writer = BitWriter::endian(&mut buffer, BigEndian);
-        BitField::write(&Some(5u8), &mut writer, ByteOrder::BigEndian, &mut (), 1).unwrap();
-        writer.byte_align().unwrap();
-        assert_eq!(vec![0x82, 0x80], buffer)
-    }
-
-    #[test]
-    fn can_write_none_bitfield() {
-        let mut buffer: Vec<u8> = Vec::new();
-        let mut writer = BitWriter::endian(&mut buffer, BigEndian);
-        BitField::write(&None::<u8>, &mut writer, ByteOrder::BigEndian, &mut (), 1).unwrap();
-        writer.byte_align().unwrap();
-        assert_eq!(vec![0x00], buffer)
+        .unwrap();
+        assert_eq!(data, vec![])
     }
 }
