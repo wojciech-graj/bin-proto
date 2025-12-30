6eb142b15beaa6033458fa3ddc858237d8be4af2
Wojciech Graj <me@w-graj.net>
2025-12-30 09:13:44 +0100

    Add test for error during array decode

--- bin-proto/src/impls/array.rs
+++ bin-proto/src/impls/array.rs
@@ -57,3 +57,53 @@ where
 
 test_codec!([u8; 4]; [0, 1, 2, 3] => [0x00, 0x01, 0x02, 0x03]);
 test_roundtrip!([u8; 4]);
+
+#[cfg(test)]
+mod test {
+    use core::cell::RefCell;
+
+    use bitstream_io::BigEndian;
+
+    use crate::{BitDecodeExt, Error};
+
+    use super::*;
+
+    #[derive(Default)]
+    struct MustDropState {
+        decoded: bool,
+        dropped: bool,
+    }
+
+    struct Ctx<'a>(&'a RefCell<MustDropState>);
+
+    struct MustDrop<'a>(&'a RefCell<MustDropState>);
+
+    impl<'a> Drop for MustDrop<'a> {
+        fn drop(&mut self) {
+            self.0.borrow_mut().dropped = true;
+        }
+    }
+
+    impl<'a> BitDecode<Ctx<'a>> for MustDrop<'a> {
+        fn decode<R, E>(_: &mut R, ctx: &mut Ctx<'a>, (): ()) -> Result<Self>
+        where
+            R: BitRead,
+            E: Endianness,
+        {
+            let mut state = ctx.0.borrow_mut();
+            if state.decoded {
+                Err(Error::Other(""))
+            } else {
+                state.decoded = true;
+                Ok(Self(ctx.0))
+            }
+        }
+    }
+
+    #[test]
+    fn partial_result_dropped() {
+        let state = RefCell::new(MustDropState::default());
+        assert!(<[MustDrop; 2]>::decode_bytes_ctx(&[], BigEndian, &mut Ctx(&state), ()).is_err());
+        assert!(state.borrow().dropped);
+    }
+}
